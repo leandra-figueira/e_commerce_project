c194e2917ca54cac58636509842695497c31d863
Leandra <leandra.figueira@gmail.com>
2018-03-13 18:32:05 -0500

    Paperclip working, Product view on ActiveAdmin updated.

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -11,5 +11,30 @@ ActiveAdmin.register Product do
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
 # end
-  permit_params :name, :description, :stock_quantity, :price, :category_id
+  permit_params :name, :description, :stock_quantity, :price, :category_id, :image
+
+  index do
+    column :name
+    column :description
+    column :stock_quantity
+    column :price
+    column "Category" do |c|
+      cat = Category.find(c.category_id)
+      link_to cat.name, admin_category_path(c.category_id)
+    end
+    column :image
+    actions
+  end
+
+  form do |f|
+    f.inputs "Product Details" do
+      f.input :name
+      f.input :description
+      f.input :stock_quantity
+      f.input :price
+      f.input :category_id, :label => 'Category', :as => :select, :collection => Category.all.collect {|category| [category.name, category.id]}
+      f.input :image, as: :file
+    end
+    f.actions
+  end
 end
