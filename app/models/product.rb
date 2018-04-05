f2e5ebb2d4adde36dedb640fc4ab5a72858d6769
Leandra <leandra.figueira@gmail.com>
2018-04-04 22:10:51 -0500

    Paperclip resizing working.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,20 @@
 class Product < ApplicationRecord
-  belongs_to :category
+
+  # Paperclip
+  has_attached_file :image,
+                    :styles => {
+                        :thumb => ["40x40%", :jpg],
+                        :small  => ["120x120%",:jpg],
+                        :medium => ["200x200%",:jpg] }
+
+
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
   validates :name, :description, :stock_quantity, :price, :category, presence: true
   validates :stock_quantity, numericality: { only_integer: true }
   validates :price, numericality: true
-  has_attached_file :image #styles: { large: "600x600>", medium: "300x300>", thumb: "150x150>" }
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
+  belongs_to :category
   has_many :order_items
   has_many :orders, through: :order_items
 
