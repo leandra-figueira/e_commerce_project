d1ffb2c8e0591880c34d3283471e3bab7d5317d3
Leandra <leandra.figueira@gmail.com>
2018-04-08 17:32:07 -0500

    Created a migration to add devise columns do customer model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180404200318) do
+ActiveRecord::Schema.define(version: 20180408222615) do
 
   create_table "abouts", force: :cascade do |t|
     t.string "description"
@@ -74,7 +74,18 @@ ActiveRecord::Schema.define(version: 20180404200318) do
     t.integer "province_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["province_id"], name: "index_customers_on_province_id"
+    t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
 
   create_table "order_items", force: :cascade do |t|
